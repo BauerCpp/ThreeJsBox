d89d3e0dd660f739e21a18b5e9b583fb4288fd0f
BauerCPP <krasilnikov03@gmail.com>
2025-02-08 11:18:46 +0300

    Release 0.0.4

--- src/entities/SkyBox.tsx
+++ src/entities/SkyBox.tsx
@@ -1,29 +1,20 @@
-import {
-  CubeTextureLoader,
-} from "three";
-import imgUrl1 from '../Texture/blue1/0001.jpg'
-import imgUrl2 from '../Texture/blue1/0002.jpg'
-import imgUrl3 from '../Texture/blue1/0003.jpg'
-import imgUrl4 from '../Texture/blue1/0004.jpg'
-import imgUrl5 from '../Texture/blue1/0005.jpg'
-import imgUrl6 from '../Texture/blue1/0006.jpg'
 
 import { useThree } from "@react-three/fiber";
-// console.log('image,', imgUrl)
-export default function SkyBox() {
-  const { scene } = useThree();
-  const loader = new CubeTextureLoader();
-  // The CubeTextureLoader load method takes an array of urls representing all 6 sides of the cube.
-  const texture = loader.load([
-    imgUrl1,
-    imgUrl2,
-    imgUrl3,
-    imgUrl4,
-    imgUrl5,
-    imgUrl6
-  ]);
+import { useEffect } from "react";
+import { CubeTexture } from "three";
 
-  // Set the scene background property to the resulting texture.
-  scene.background = texture;
-  return null;
+
+interface SkyBoxProps {
+	texture: CubeTexture;
 }
+
+const SkyBox: React.FC<SkyBoxProps> = ({ texture }) => {
+	const { scene } = useThree();
+	useEffect( () =>
+	{
+		scene.background = texture;
+	}, [scene, texture])
+	return null;
+};
+
+export default SkyBox;
